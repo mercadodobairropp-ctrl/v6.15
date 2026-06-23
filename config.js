f43d9454d7ecca39c0e4069876a111e59e773286
mercadodobairropp-ctrl <mercadodobairropp@gmail.com>
2026-06-23 16:54:24 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 ﻿const APP = {
   versao: "v6.15",
-  API_URL: "https://script.google.com/macros/s/AKfycbxFmSDYg6xyLKCCXmPE2OoBpRZlPcg0lA7qALKMJLTINPCmxRw-IoU__BoTpXKhdwVH/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbw8efsu7_JEybMoxJbvt0gjn1yAHICY7HFSl0ZVl5OM3jl8iyWDk9Xz2_Zs7XgXD1Ir/exec",
   UPLOAD_API_URL: "https://script.google.com/macros/s/AKfycbzkB5dLtdWDmm9NRhRnktqta69-Lvf_ix1H80-B8YGLf1d8_ap1_WH-FQOjweHrDXJ0Dg/exec",
   ADMIN_MESTRE: "01",
   CACHE_MS: 45000,
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME="painel-operacional-v6-15-device-20260623";
+﻿const CACHE_NAME="painel-operacional-v6-15-api-20260623";
 const ASSETS=["./","index.html","login.html","painel.html","checklist.html","monitor.html","styles.css","config.js","app.js","manifest.json","icon-192.png","icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
@@ -19,5 +19,6 @@ async function navigation(req){
   }
 }
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const url=new URL(e.request.url);if(url.origin!==self.location.origin){e.respondWith(fetch(e.request));return}if(e.request.mode==="navigate"){e.respondWith(navigation(e.request));return}e.respondWith(cacheFirst(e.request))});
+
 
 
